d924d39935dd2a09f1e2a64d0b753edf35001656
sanmisanti <sanmisanti@gmail.com>
2024-02-07 12:33:04 -0300

    Export to Excel

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,37 @@
 import React, { useContext } from 'react';
 import { CartContext } from './Context/CartProvider';
 import { Button, Col, Container, Row, Table } from 'react-bootstrap';
+import { exportFileXLSX } from '../utils/Excelexport';
 
 const Cart = () => {
 	const { cart, addToCart, clearCart } = useContext(CartContext);
+
+	const exportFile = () => {
+		/* const cartToExport = cart.map(item => {
+			const Codigo = `${item.codClase}-${item.itemCod}`;
+
+			// Crear un nuevo objeto con 'codigo' como la primera propiedad
+			const newItem = {
+				Codigo,
+				Descripcion: item.descripcion,
+				Observacion: item.observacion,
+			};
+
+			// Eliminar las propiedades no deseadas
+			delete newItem.id;
+			delete newItem.codClase;
+			delete newItem.esActivo;
+			delete newItem.itemCod;
+			delete newItem.lastModified;
+			delete newItem.Clases;
+
+			return newItem;
+		}); */
+
+		// exportToExcel(cartToExport, 'Items_Template.xlsx');
+
+		exportFileXLSX(cart);
+	};
 
 	return (
 		<Container>
@@ -12,8 +40,8 @@ const Cart = () => {
 				<thead>
 					<tr className='align-middle text-center'>
 						<th className='col-1'>Codigo</th>
-						<th className='col-1'>Clase</th>
-						<th className='col-9'>Descripcion</th>
+						<th className='col-'>Descripcion</th>
+						<th className='col-1'>Eliminar</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -22,21 +50,36 @@ const Cart = () => {
 							<tr
 								key={item.id}
 								className='align-middle text-center table-success'
-								onClick={() => addToCart(item)}
 							>
 								<td>{`${item.codClase}-${item.itemCod}`}</td>
-								<td>{item.Clases.descripcion}</td>
-
 								<td className='text-start pl-1'>{item.descripcion}</td>
+								<td>
+									<Button
+										variant='danger'
+										onClick={() => addToCart(item)}
+										className='mb-2'
+									>
+										Eliminar
+									</Button>
+								</td>
 							</tr>
 						);
 					})}
 				</tbody>
 			</Table>
 			{cart.length > 0 && (
-				<Button className='mb-2' onClick={() => clearCart()}>
-					Limpiar Carrito
-				</Button>
+				<div>
+					<Button className='mb-2 me-3' onClick={() => clearCart()}>
+						Limpiar Carrito
+					</Button>
+					<Button
+						variant='success'
+						className='mb-2'
+						onClick={() => exportFile()}
+					>
+						Exportar
+					</Button>
+				</div>
 			)}
 		</Container>
 	);
